891b75f39c5f283957c8ecfd03fabd11fa4bed76
abidkhan03 <khansahb5566@gmail.com>
2023-01-09 11:41:34 +0500

    first commit

---
+++ httpserver/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/hello", HelloHandler)
+	fmt.Println("running server ")
+	http.ListenAndServe(":8080", nil)
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!\n", r.URL.Path[1:])
+}
---
+++ httpserver/readme.md
@@ -0,0 +1,21 @@
+# Simple http server in Golang
+
+### build a script
+
+if it requires perssion then 
+
+```chmod +x scripts/build.sh```
+
+then type
+
+```scripts/build.sh```
+
+### run a shel to run the program
+
+Sometimes permission requires
+
+```chmod +x scripts/run.sh```
+
+then
+
+```scripts/run.sh```
