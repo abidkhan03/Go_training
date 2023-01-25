0325f53d440ddf9f34ac15ab1e323da4814bbb37
abidkhan03 <abidkhan42935@gmail.com>
2023-01-25 11:38:04 +0500

    Changed Fmt to Log and Handled Errors in Gorm File

--- db/db.go
+++ db/db.go
@@ -11,7 +11,7 @@ var err error
 
 func init() {
 	dbURL := "postgres://postgres:postgres@localhost:5432/postgres?dbname"
-	
+
 	DB, err = gorm.Open(postgres.Open(dbURL), &gorm.Config{})
 	if err != nil {
 		panic(err)
--- db/gorm.go
+++ db/gorm.go
@@ -2,7 +2,7 @@ package db
 
 import (
 	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 
@@ -18,9 +18,8 @@ type Object struct {
 
 func (h handler)GetAllObjects(w http.ResponseWriter, r *http.Request) {
 	var objects []Object
-	
 	if result := h.DB.Find(&objects); result.Error != nil {
-		fmt.Println(result.Error)
+		log.Println(result.Error)
 	}
 
 	w.Header().Add("Content-Type", "application/json")
@@ -30,10 +29,9 @@ func (h handler)GetAllObjects(w http.ResponseWriter, r *http.Request) {
 
 func (h handler) GetObjectByID(w http.ResponseWriter, r *http.Request) {
 	var object Object
-	vars := chi.URLParam(r, "id")
-	id := vars
+	id := chi.URLParam(r, "id")
 	if result := h.DB.First(&object, id); result.Error != nil {
-		fmt.Println(result.Error)
+		log.Println(result.Error)
 	}
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
@@ -44,7 +42,7 @@ func (h handler) CreateObject(w http.ResponseWriter, r *http.Request) {
 	var object Object
 	json.NewDecoder(r.Body).Decode(&object)
 	if result := h.DB.Create(&object); result.Error != nil {
-		fmt.Println(result.Error)
+		log.Println(result.Error)
 	}
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
@@ -61,16 +59,17 @@ func (h handler) UpdateObjectByID(w http.ResponseWriter, r *http.Request) {
 	json.NewDecoder(r.Body).Decode(&updatedObject)
 
 	var object Object
-
 	if result := h.DB.First(&object, id); result.Error != nil {
-		fmt.Println(result.Error)
+		log.Println(result.Error)
 	}
-
 	object.ID = updatedObject.ID
 	object.Name = updatedObject.Name
 	object.Description = updatedObject.Description
 
-	h.DB.Save(&object)
+	// check if it returns an error
+	if result := h.DB.Save(&object); result.Error!= nil {
+        log.Println(result.Error)
+    }
 
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
@@ -80,17 +79,13 @@ func (h handler) UpdateObjectByID(w http.ResponseWriter, r *http.Request) {
 func (h handler) DeleteObjectByID(w http.ResponseWriter, r *http.Request) {
 	vars := chi.URLParam(r, "id")
 	id, _ := strconv.Atoi(vars)
-
 	var object Object
-
 	if result := h.DB.First(&object, id); result.Error != nil {
-		fmt.Println(result.Error)
+		log.Println(result.Error)
 		w.WriteHeader(http.StatusNotFound)
         return
     }
-	
 	h.DB.Delete(&object)
-
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode("Deleted")
