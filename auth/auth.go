2d691a595fc6466dbe05c50b7c4cc36acb6b97a1
abdullah5abid <abdullah@webilogics.com>
2023-01-26 11:09:47 +0500

    Sepereated SignIn Function from Jwt File into Auth Go File

---
+++ auth/auth.go
@@ -0,0 +1,38 @@
+package auth
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	abc "github.com/dgrijalva/jwt-go"
+)
+
+var byte_key = []byte("secret")
+
+func SignIn(w http.ResponseWriter, r *http.Request) {
+	var user User
+	// receive user oject in request
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	claims := &abc.MapClaims{
+		"username": user.Username,
+		"exec":     time.Now().Add(time.Hour + time.Minute),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// Create the JWT string
+	tokenString, err := token.SignedString(byte_key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(tokenString))
+}
--- auth/jwt.go
+++ auth/jwt.go
@@ -1,13 +1,8 @@
 package auth
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
-	"time"
 
-	"github.com/dgrijalva/jwt-go"
-	abc "github.com/dgrijalva/jwt-go"
 	"github.com/go-chi/jwtauth/v5"
 )
 
@@ -22,49 +17,27 @@ func init() {
 	TokenAuth = jwtauth.New("HS256", []byte("secret"), nil)
 }
 
-func SignIn(w http.ResponseWriter, r *http.Request) {
-	var user User
-	// receive user oject in request
-	err := json.NewDecoder(r.Body).Decode(&user)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	claims := &abc.MapClaims{
-		"username": user.Username,
-		"exec":     time.Now().Add(time.Hour),
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	// Create the JWT string
-	tokenString, err := token.SignedString([]byte("secret"))
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(tokenString))
-}
-
 func ValidateToken(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		_, claims, err := jwtauth.FromContext(r.Context())
 		if err != nil {
-			next.ServeHTTP(w, r)
+			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-		
+
 		if claims["username"] == "" {
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintf(w, `{"error": "invalid credentials"}`)
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
 			return
 		}
+		// check expiration time
+		// var expire_time = 300
+		// if claims["exec"].(int64) < int64(expire_time) {
+		// 	http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		// 	return
+		// }
+
 		if claims["username"] == "Ali" {
 			next.ServeHTTP(w, r)
-			return
 		}
 	})
 }
